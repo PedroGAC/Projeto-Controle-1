b42474e3d8cb7c8786f5309689e1a7f22e8af64b
PedroGAC <pedro.cruz@estudante.cear.ufpb.br>
2024-10-09 11:00:17 -0300

    Códigos faltantes

--- identification_fs.m
+++ identification_fs.m
@@ -16,20 +16,15 @@
 % % Abrir o aplicativo 'ident' para trabalhar com a identificação de sistemas
 % ident
 
-% Carregar os dados de PWM (Entrada) e RPM (Saída)
-pwm_data = load('pwm_novo.txt');  % Substitua com o caminho correto para o arquivo
-rpm_data = load('rpm_novo.txt');  % Substitua com o caminho correto para o arquivo
+pwm_data = load('pwm_novo.txt'); 
+rpm_data = load('rpm_novo.txt');  
 
-% Separar os dados em tempo e valores
-tempo_pwm = pwm_data(:, 1);  % A primeira coluna é o tempo
-valores_pwm = pwm_data(:, 2);  % A segunda coluna é o valor de PWM
+tempo_pwm = pwm_data(:, 1); 
+valores_pwm = pwm_data(:, 2); 
 
-tempo_rpm = rpm_data(:, 1);  % A primeira coluna é o tempo
-valores_rpm = rpm_data(:, 2);  % A segunda coluna é o valor de RPM
+tempo_rpm = rpm_data(:, 1); 
+valores_rpm = rpm_data(:, 2); 
 
-% Criar um objeto de dados de identificação de sistemas (iddata)
-% O tempo de amostragem é inferido a partir dos dados, mas você pode definir explicitamente
 data = iddata(valores_rpm, valores_pwm, 1);
 
-% Abrir o aplicativo 'ident' para trabalhar com a identificação de sistemas
 ident
